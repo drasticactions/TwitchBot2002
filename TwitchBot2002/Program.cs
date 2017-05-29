1074d9363da1d9ebfab993587363f72e83892c90
Tim Miller <innerlogic4321@gmail.com>
2017-05-29 17:40:24 -0400

    Add DLLs, Joypad support

--- TwitchBot2002/Program.cs
+++ TwitchBot2002/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using TwitchLib;
 using TwitchLib.Models.Client;
 using System.Speech.Synthesis;
+using vJoyInterfaceWrap;
 
 namespace TwitchBot2002
 {
@@ -14,14 +15,49 @@ namespace TwitchBot2002
 
         static SpeechSynthesizer synth;
 
+        static public vJoy joystick;
+        static public vJoy.JoystickState iReport;
+        static public uint id = 1;
+
         static void Main(string[] args)
         {
-            MainAsync().GetAwaiter().GetResult();
+            MainAsync(args).GetAwaiter().GetResult();
         }
 
-        static async Task MainAsync()
+        static async Task MainAsync(string[] args)
         {
+            #region Setup VJoy
+
+            joystick = new vJoy();
+            iReport = new vJoy.JoystickState();
+
+            if (args.Length > 0 && !String.IsNullOrEmpty(args[0]))
+                id = Convert.ToUInt32(args[0]);
+
+            if (!joystick.vJoyEnabled())
+            {
+                Console.WriteLine("vJoy driver not enabled: Failed Getting vJoy attributes.\n");
+                return;
+            }
+
+            // Get the state of the requested device
+            VjdStat status = joystick.GetVJDStatus(id);
+
+            if ((status == VjdStat.VJD_STAT_OWN) || ((status == VjdStat.VJD_STAT_FREE) && (!joystick.AcquireVJD(id))))
+            {
+                Console.WriteLine("Failed to acquire vJoy device number {0}.\n", id);
+                return;
+            }
+
+            joystick.ResetVJD(id);
+            joystick.ResetButtons(id);
+
+            #endregion
+
             synth = new SpeechSynthesizer();
+            synth.SpeakStarted += Synth_SpeakStarted;
+            synth.SpeakCompleted += Synth_SpeakCompleted;
+
             synth.SetOutputToDefaultAudioDevice();
 
             var authTokens = System.IO.File.ReadLines("tokens.txt").ToArray();
@@ -29,8 +65,26 @@ namespace TwitchBot2002
             var client = new TwitchClient(new ConnectionCredentials(authTokens[0], authTokens[1]), "");
             client.OnMessageReceived += Client_OnMessageReceived;
             client.Connect();
-            Console.WriteLine("Press any key to quit");
-            Console.ReadKey();
+            while(true)
+            {
+                var userMessage = Console.ReadLine();
+                SpeakAsync(userMessage);
+            }
+        }
+
+        private static async void SpeakAsync(string message)
+        {
+            synth.SpeakAsync(message);
+        }
+
+        private static void Synth_SpeakStarted(object sender, SpeakStartedEventArgs e)
+        {
+            joystick.SetBtn(true, id, 1);
+        }
+
+        private static void Synth_SpeakCompleted(object sender, SpeakCompletedEventArgs e)
+        {
+            joystick.SetBtn(false, id, 1);
         }
 
         private static async void Client_OnMessageReceived(object sender, TwitchLib.Events.Client.OnMessageReceivedArgs e)
@@ -38,7 +92,7 @@ namespace TwitchBot2002
             if (e.ChatMessage.Bits > 0)
             {
                 // voice message!
-                synth.SpeakAsync(e.ChatMessage.Message);
+                SpeakAsync(e.ChatMessage.Message);
                 Console.WriteLine($"Voice: {e.ChatMessage.Username}: {e.ChatMessage.Message}");
             }
             else
