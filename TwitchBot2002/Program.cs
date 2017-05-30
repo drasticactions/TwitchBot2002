8f62c625e7dd6455c25c387cd9ad475237a9a779
Tim Miller <innerlogic4321@gmail.com>
2017-05-29 20:16:11 -0400

    Change to use NAudio

--- TwitchBot2002/Program.cs
+++ TwitchBot2002/Program.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 using TwitchLib;
 using TwitchLib.Models.Client;
 using System.Speech.Synthesis;
+using System.Speech;
 using vJoyInterfaceWrap;
+using System.IO;
+using System.Speech.AudioFormat;
+using NAudio.Wave;
+using NAudio.CoreAudioApi;
 
 namespace TwitchBot2002
 {
@@ -15,9 +20,14 @@ namespace TwitchBot2002
 
         static SpeechSynthesizer synth;
 
-        static public vJoy joystick;
-        static public vJoy.JoystickState iReport;
-        static public uint id = 1;
+        static vJoy joystick;
+        static vJoy.JoystickState iReport;
+        static uint id = 1;
+
+        static BufferedWaveProvider bwp;
+
+        static WaveInEvent waveIn;
+        static WaveOut waveOut;
 
         static void Main(string[] args)
         {
@@ -26,6 +36,34 @@ namespace TwitchBot2002
 
         static async Task MainAsync(string[] args)
         {
+            #region Audio
+
+            for (int i = 0; i < WaveOut.DeviceCount; i++)
+            {
+                WaveOutCapabilities WOC = WaveOut.GetCapabilities(i);
+                Console.WriteLine($"{i + 1}: {WOC.ProductName}");
+            }
+
+            Console.WriteLine("Enter Audio Output Device Number: ");
+            var deviceNumString = Console.ReadLine();
+            var deviceInt = 0;
+            try
+            {
+                deviceInt = Convert.ToInt32(deviceNumString) - 1;
+                if (deviceInt < 0 || deviceInt > WaveOut.DeviceCount - 1) return;
+            }
+            catch (Exception)
+            {
+                return;
+            }
+
+            waveOut = new WaveOut(WaveCallbackInfo.FunctionCallback());
+            waveOut.DeviceNumber = deviceInt;
+        
+            waveOut.PlaybackStopped += WaveOut_PlaybackStopped;
+
+            #endregion
+
             #region Setup VJoy
 
             joystick = new vJoy();
@@ -55,36 +93,51 @@ namespace TwitchBot2002
             #endregion
 
             synth = new SpeechSynthesizer();
-            synth.SpeakStarted += Synth_SpeakStarted;
-            synth.SpeakCompleted += Synth_SpeakCompleted;
 
-            synth.SetOutputToDefaultAudioDevice();
+            var authTokens = File.ReadLines("tokens.txt").ToArray();
 
-            var authTokens = System.IO.File.ReadLines("tokens.txt").ToArray();
             // Bot Username, API Key, Channel to join.
             var client = new TwitchClient(new ConnectionCredentials(authTokens[0], authTokens[1]), "");
             client.OnMessageReceived += Client_OnMessageReceived;
             client.Connect();
+            Console.WriteLine("Client Started! Use CTRL-Z to quit");
             while(true)
             {
                 var userMessage = Console.ReadLine();
-                SpeakAsync(userMessage);
+                Speak(userMessage);
             }
         }
 
-        private static async void SpeakAsync(string message)
-        {
-            synth.SpeakAsync(message);
-        }
-
-        private static void Synth_SpeakStarted(object sender, SpeakStartedEventArgs e)
-        {
-            joystick.SetBtn(true, id, 1);
-        }
-
-        private static void Synth_SpeakCompleted(object sender, SpeakCompletedEventArgs e)
+        private static void WaveOut_PlaybackStopped(object sender, StoppedEventArgs e)
         {
             joystick.SetBtn(false, id, 1);
+        }
+
+        private static void Speak(string message)
+        {
+            joystick.SetBtn(true, id, 1);
+            WaveFileReader reader = new WaveFileReader(new MemoryStream(TextToBytes(message)));
+            waveOut.Init(reader);
+            waveOut.Play();
+        }
+
+        public static byte[] TextToBytes(string textToSpeak)
+        {
+            byte[] byteArr = null;
+
+            var t = new System.Threading.Thread(() =>
+            {
+                SpeechSynthesizer ss = new SpeechSynthesizer();
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    ss.SetOutputToWaveStream(memoryStream);
+                    ss.Speak(textToSpeak);
+                    byteArr = memoryStream.ToArray();
+                }
+            });
+            t.Start();
+            t.Join();
+            return byteArr;
         }
 
         private static async void Client_OnMessageReceived(object sender, TwitchLib.Events.Client.OnMessageReceivedArgs e)
@@ -92,12 +145,12 @@ namespace TwitchBot2002
             if (e.ChatMessage.Bits > 0)
             {
                 // voice message!
-                SpeakAsync(e.ChatMessage.Message);
+                Console.WriteLine($"Voice: {e.ChatMessage.Username} ({e.ChatMessage.Bits}: {e.ChatMessage.Message}");
+                //Console.WriteLine("Allow? y/n");
+                //var key = Console.ReadKey();
+                //if (key.Key != ConsoleKey.Y) return;
+                Speak(e.ChatMessage.Message);
                 Console.WriteLine($"Voice: {e.ChatMessage.Username}: {e.ChatMessage.Message}");
-            }
-            else
-            {
-                Console.WriteLine($"{e.ChatMessage.Username}: {e.ChatMessage.Message}");
             }
         }
     }
